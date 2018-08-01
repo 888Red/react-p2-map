2e6fc28d545d3ebd74d89e0bb480c9fd32d19593
Mildred Pastryk <nicole.pards@gmail.com>
2018-08-01 22:30:47 +0200

    feat: mount and initialize google map

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function loadMapJS(src) {
+   let ref = window.document.getElementsByTagName('script')[0];
+   let script = window.document.createElement('script');
+
+   script.src = src;
+   script.async = true;
+   ref.parentNode.insertBefore(script, ref);
+
+   script.onerror = function () {
+   document.write('Google Map is not loading')
+ };
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+   componentDidMount() {
+    window.initMap = this.initMap;
+    loadMapJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyAQjfF9VNsHrOWzGs8IE9wtU96XbMJwcEU&callback=initMap');
+  }
+   initMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: { lat: 41.9035801, lng: 12.4500056 }
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Neighborhood Map Project (React)</h1>
+          <h1 className="App-title">FEND Project: Vatican Map</h1>
         </header>
+        <div id="map">
+        </div>
       </div>
     );
   }
