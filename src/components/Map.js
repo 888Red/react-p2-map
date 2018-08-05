9625a7bbdfc5b592179386d62614d7f6f7feeb00
Mildred Pastryk <nicole.pards@gmail.com>
2018-08-05 21:08:46 +0200

    feat: infowindow details from foursquare

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import Header from './Header.js'
+import foursquare from '../img/foursquare.png'
 
 /* Functions */
+function InfoWindowDetails(props) {
+  return (
+    <React.Fragment>
+      <h3>{props.object.venue.name}</h3>
+      <h4>Address:</h4><p>{props.object.venue.location.formattedAddress[0]}</p>
+      <p>{props.object.venue.location.formattedAddress[1]}</p>
+      <img className="foursquare" src={foursquare} alt="powered by foursquare" />
+    </React.Fragment>
+  )
+}
 
 function createURL(fsURL, params) {
   const paramString = new URLSearchParams(params);
@@ -48,13 +60,14 @@ class Map extends Component {
     this.setState({map}, this.getFsVenues)
   }
 
-  /* Use Foursquare API to get venues - fountains around Rome */
+  /* Use Foursquare API to get top venues using explore endpoint - fountains around Rome */
   getFsVenues() {
     const fsURL = 'https://api.foursquare.com/v2/venues/explore?'
     const params = {
       query: 'fountain',
       limit: '25',
       near: 'Rome, Italy',
+      radius: 12000,
       client_id: 'O51T00R0EHFYS3CDFIMKMN232SX53A5KKGU02I1LQT5DWB1I',
       client_secret: 'URFU1AYKAJMXKIF5OMZWYZJJ24T1FOO3BCSQ20QBSTDDAEO1',
       v: '20180802'
@@ -81,6 +94,7 @@ class Map extends Component {
         this.setState({locations: locationsAll})
     }
 
+    /* Add InfoWindow and Markers*/
     addInfoWindow(object, position) {
       const infoWindow = new window.google.maps.InfoWindow();
       infoWindow.setPosition(position);
@@ -105,16 +119,47 @@ class Map extends Component {
       object.venue.marker = marker;
     }
 
-  render() {
-    return (
-      <div>
-        <Header/>
-        <div id="map" ref={this.mapElement}></div>
-        {this.state.fetchError && <FetchError />}
-      </div>
-    );
+    /*  Render updates of InfoWindow and Markers */
+
+    openInfoWindow(object, position, map, marker) {
+      const updateInfoWindow = this.state.locations.map(location => {
+        if (location.venue.id === object.venue.id) {
+          if (location.venue.infoWindow.map) {
+              location.venue.infoWindow.close();
+              return location;
+            } else {
+              this.addWindowDetails(location);
+              location.venue.infoWindow.open(map, marker);
+              return location;
+            }
+          } else {
+            location.venue.infoWindow.close();
+            return location;
+          }
+        })
+        map.panTo(position);
+        this.setState({locations: updateInfoWindow})
+    }
+
+    addWindowDetails(object) {
+      const div = document.createElement('div');
+      div.className = 'info-window';
+      ReactDOM.render(<InfoWindowDetails object={object} />, div)
+      object.venue.infoWindow.setContent(div);
   }
 
+  render() {
+  return (
+    <div>
+      <Header/>
+
+      <div
+        id="map"
+        ref={this.mapElement}></div>
+      {this.state.fetchError && <FetchError />}
+    </div>
+  )
+}
 }
 
 export default Map
